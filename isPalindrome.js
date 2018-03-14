7ba46f05b4272e4030eb1132cac3d5ad783059ad
Alex Lisii <alexandru.lisii@gmail.com>
2018-03-14 11:27:12 -0700

    add file

---
+++ isPalindrome.js
@@ -0,0 +1,28 @@
+function isPalindrome(inputString){
+  
+  let hashMap = {};
+  let stringArray = inputString.split("");
+  
+  // create a hash map
+  // count how many times each char appears
+  stringArray.forEach(function(char){
+    
+    // char is not yet in the hashMap
+    if (typeof(hashMap[char]) === "undefined"){
+      hashMap[char] = 1;
+    }
+    
+    // char present in the hashMap, increment by 1
+    else {
+      hashMap[char] += 1;
+    }
+  });
+  
+  // if you have odd chars more than 1 time, there is no way to 
+  // have a palindrome
+  let oddLetters = Object.values(hashMap).filter(value => value % 2 !== 0);
+  
+  if (oddLetters.length > 1) 
+    return false;
+  return true;
+}
